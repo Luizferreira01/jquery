d662b83c49ff656abccafed3247d36fe5147f00c
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-01-05 18:40:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-$(document).ready(function() {
-    $('header button').click(function(){
-        alert("campo cadastrado")
-    })
+const form = document.getElementById('form');
+const nometarefa = [];
+let linhas = '';
 
-    $('form').on('submit', function(e){
-        console.log("submit");
-        e.preventDefault();
-    })
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    addtarefa();
+    atualizatabela();
+
 })
+
+function addtarefa() {
+    const inputtarefa = document.getElementById('tarefa');
+
+    if(nometarefa.includes(inputtarefa.value)){
+        alert(`a tarefa ${inputtarefa.value}ja foi inserida!`);
+    }else{
+        nometarefa.push(inputtarefa.value);
+
+        linha += `<li> ${inputtarefa.value}</li>`;
+
+        linhas += linha:
+    }
+
+    inputtarefa.value = '';
+}
+
+function atualizatabela(){
+    const tabelaraiz = document.querySelector('ul');
+    tabelaraiz.addEventListener('click', function (e){
+        if(e.targetclasslist.contains('checked')){
+            e.target.classlist.remove('checked');
+        }else{
+            e.target.classlist.add('checked');
+        }
+    });
+}
