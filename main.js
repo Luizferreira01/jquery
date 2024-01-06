4b3d8f0b8f375d51caef97f3dbac5ab5a3bf8420
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-01-06 13:06:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,34 +5,14 @@ let linhas = '';
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    addtarefa();
-    atualizatabela();
+const inputnometarefa = document.getElementById('nome-tarefa');
 
+let linha = '<li>';
+linha += `<ul>${inputnometarefa.value}</ul>`;
+linha += '</li>';
+
+const corpotabela = document.querySelector('tbody');
+corpotabela.insertAdjacentHTML('beforeend', linha);
+
+inputnometarefa.value = '';
 })
-
-function addtarefa() {
-    const inputnometarefa = document.getElementById('nometarefa');
-
-    if(nometarefa.includes(inputnometarefa.value)){
-        alert(`a tarefa ${inputnometarefa.value} ja foi inserida!`);
-    }else{
-        nometarefa.push(inputnometarefa.value);
-
-        var linha = `<li> ${inputnometarefa.value}</li>`;
-
-        linhas += linha;
-    }
-
-    inputnometarefa.value = '';
-}
-
-function atualizatabela(){
-    const tabelaraiz = document.querySelector('ul');
-    tabelaraiz.addEventListener('click', function (e){
-        if(e.targetclasslist.contains('checked')){
-            e.target.classlist.remove('checked');
-        }else{
-            e.target.classlist.add('checked');
-        }
-    });
-}
