35d9257cce46cf561622845a08f3289aef3fd461
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-01-08 20:40:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 $(document).ready(function(){
-    $('header button').click(function(){
-        alert("tarefa registrada")
-    })
-    
     $('form').on('submit', function(e){
         e.preventDefault();
-        const tarefa = $('#tarefa').val();
-        const novatarefa = $ ('<li></li>');
-        $(`<div class="tarefas">
-            <a href="${tarefa}" target="_blank" title="novatarefa">
-                novas tarefas
-            </a>
-        </div>
-        `).appendTo(novatarefa);
-        $(novatarefa).appendTo('ul');
-        $(novatarefa).fadein(5000);
-        $('tarefa').val('')
+        const nomeDaTarefa = $('input').val();
+        const novaTarefa = $(`<li>${nomeDaTarefa}</li>`)
 
-
+        $(novaTarefa).appendTo('ul')
+        $('input').val('')
     })
 
+    $('ul').on('click', 'li', function(e){
+        e.preventDefault();
+        $(this).toggleClass('riscartarefa')
+    })
 })
