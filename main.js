37772d165ef8fc1fb7b677374b80e5de83f61d25
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-01-08 19:34:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-const form = document.getElementById('form-lista');
-const NomeTarefa = [];
-let linhas = '';
-
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    AddTarefa();
-    atualizaTabela();
+$(document).ready(function(){
+    $('header button').click(function(){
+        alert("tarefa registrada")
+    })
     
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const tarefa = $('#tarefa').val();
+        const novatarefa = $ ('<li></li>');
+        $(`<div class="tarefas">
+            <a href="${tarefa}" target="_blank" title="novatarefa">
+                novas tarefas
+            </a>
+        </div>
+        `).appendTo(novatarefa);
+        $(novatarefa).appendTo('ul');
+        $(novatarefa).fadein(5000);
+        $('tarefa').val('')
+
+
+    })
+
 })
-
-function AddTarefa() {
-    const InputTarefa = document.getElementById('Tarefa');
-
-    if(NomeTarefa.includes(InputTarefa.value)){
-        alert(`a tarefa ${InputTarefa.value} já foi inserida!`);
-    }else {
-    
-    NomeTarefa.push(InputTarefa.value);
-
-  
-    let linha = `<li> ${InputTarefa.value}</li>`;
-
-    linhas += linha;
-    }
-    
-    InputTarefa.value = '';
-}
-
-function atualizaTabela(){
-    const Tabelaraiz = document.querySelector('ul');
-    Tabelaraiz.innerHTML = linhas;
-}
